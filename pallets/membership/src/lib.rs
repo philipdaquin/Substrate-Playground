5195da6fb58007483060ef30caec1b2b9200a91d
philidaquin <philipdaquin20@gmail.com>
2021-12-11 18:53:48 +0800

    Runtime Development Done Testing in progress

--- pallets/membership/src/lib.rs
+++ pallets/membership/src/lib.rs
@@ -323,5 +323,132 @@ use frame_support::{dispatch::DispatchResult, pallet_prelude::*, traits::{Change
 			Ok(())
 		}
 	} 	
+	impl<T: Config> Pallet<T> { 
+		pub(crate) fn do_add_role(
+			role: Role,
+		) -> bool { 
+			let all_roles = Roles::<T>::get();
+			
+			if all_roles.contains(&role) { 
+			   return  false
+			}
+			Roles::<T>::append(role.clone());
+			true 
+	
+		}
+		pub(super) fn verify_manage_access(
+			pallet_name: Vec<u8>,
+			acc: T::AccountId, 
+		) -> bool { 
+			let all_roles = Roles::<T>::get();
+			let role = Role { 
+				pallet_name,
+				permission: Permissions::Management
+			};
+			if all_roles.contains(&role) && Permission::<T>::get((acc, role.clone() )) { 
+				return true
+			}
+			false
+		}
+		pub fn verify_access(
+			acc: T::AccountId, 
+			pallet_name: Vec<u8>,
+		) -> bool { 
+			let role_management = Role { 
+				pallet_name: pallet_name.clone(),
+				permission: Permissions::Management
+			};
+			let role_executor = Role { 
+				pallet_name: pallet_name.clone(),
+				permission: Permissions::Executors
+			};
+			let all_roles = Roles::<T>::get();
+	
+			let management = Permission::<T>::get((acc.clone(), role_management.clone()));
+			let executor = Permission::<T>::get((acc.clone(), role_executor.clone()));
+			
+			if all_roles.contains(&role_management) && management || all_roles.contains(&role_executor) && executor { 
+				return true
+			}
+			return  false
+		}
+	}
+	
+	// Signed Transactions 
+	#[derive(Encode, Decode, Clone, Eq, PartialEq, scale_info::TypeInfo)]
+	pub struct Authorize<T: Config + Send + Sync>(PhantomData<T>);
+	
+	/// Debug impl for the `Authorize` struct.
+	impl<T: Config + Send + Sync> Debug for Authorize<T> {
+		#[cfg(feature = "std")]
+		fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
+			write!(f, "Authorize")
+		}
+	
+		#[cfg(not(feature = "std"))]
+		fn fmt(&self, _: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
+			Ok(())
+		}
+	}
+	
+	
+	impl<T: Config + Send + Sync> SignedExtension for Authorize<T> where 
+		T::Call: Dispatchable<Info = DispatchInfo> + GetCallMetadata, T: scale_info::TypeInfo
+	{ 
+		type AccountId = T::AccountId; 
+		// The type which encodes the sender identity 
+		type Call = T::Call;
+		//	the type which encodes the call to be dispatched 
+		type AdditionalSigned = ();
+		//	any additional adata that will go into the signed payload
+		type Pre = ();
+		//	the type that encodes information that can be passed from pre-dispatch to post dispatch
+		const IDENTIFIER: &'static str = "Authorize";
+		//	The unique identifier of this signed extension 
+	
+		//	 Construct any additional data that should be in the signed payload of the transaction 
+		//	This will perform anyu pre-signature verification checks and returns and error if needed
+		fn additional_signed(&self) -> sp_std::result::Result<(), TransactionValidityError> {
+			Ok(())
+		}
+	
+		//	Validate a signed transaction for the transaction queue
+		fn validate( 
+			&self, 
+			who: &Self::AccountId,
+			call: &Self::Call,
+			info: &DispatchInfoOf<Self::Call>,
+			_len: usize, 
+		) -> TransactionValidity { 
+			let md = call.get_call_metadata();
+			
+			//	Check Transaction Queues for Admin extrinsics, don't filter ones with key access
+			//	Check if Signed Transactions ahve Admin Keys 
+			if Admin::<T>::contains_key(who.clone()) { 
+				print!("Access Granted!");
+	
+				Ok(ValidTransaction { 
+					priority: info.weight as TransactionPriority,
+					longevity: TransactionLongevity::max_value(),
+					propagate: true, 
+					..Default::default()
+				})
+			//	Check if Signed Transactions have Verified Access 
+			} else if Pallet::<T>::verify_access(who.clone(), md.pallet_name.as_bytes().to_vec()) { 
+				print!("Access Granted");
+	
+				Ok(ValidTransaction { 
+					priority: info.weight as TransactionPriority,
+					longevity: TransactionLongevity::max_value(),
+					propagate: true, 
+					..Default::default()
+				})
+	
+			} else { 
+				print!("Access Denied");
+				Err(InvalidTransaction::Call.into())
+			}
+		}
+	}
 	
 }
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -6,6 +6,8 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
+use frame_benchmarking::baseline::mock::OriginCaller;
+use frame_support::PalletId;
 use pallet_grandpa::{
 	fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
 };
@@ -49,6 +51,7 @@ pub use pallet_template;
 pub use pallet_recurringpayments;
 pub use pallet_proxy;
 pub use pallet_scheduler;
+pub use pallet_collective;
 // 
 
 /// An index to a block.
@@ -290,31 +293,153 @@ impl pallet_template::Config for Runtime {
 	type Event = Event;
 }
 
+type EnsureRootOrHalfCouncil = EnsureOneOf<
+	EnsureRoot<AccountId>,
+	pallet_collective::EnsureProportionMoreThan<_1, _2, AccountId, CouncilCollective>,
+>;
+
 // =============> Pallet Recurring Payments Traits <=============// 
 parameter_types! {
+	pub const RecurringPaymentPalletId: PalletId = PalletId(b"PaymentSystem"); 
+	pub const StringLimit: u32 = 50;
 
 }
 
 impl pallet_recurringpayments::Config for Runtime { 
+	type Event = Event; 
+	type Currency = Balances;
+	type Moment = u64;
+	type Info = Call;
+	type ForceOrigin = EnsureRootOrHalfCouncil;
+	type SubmissionDeposit = SubmissionDeposit;
+	type PalletsOrigin = OriginCaller;
+	type Scheduler = EnsureRoot<AccountId>;
+	type ForceOrigin = EnsureRootOrHalfCouncil;
+	type Moment = Moment;
 
 }
 
-// ========> Pallet Proxy <=================//
+// ===============> Pallet Proxy <=====================//
 parameter_types! {
-
-}
-impl pallet_proxy::Config for Runtime { 
-
-}
-// =======> Pallet Scheduler <============//
-parameter_types! { 
-
-}
-impl pallet_scheduler::Config for Runtime { 
-
+	// One storage item; key size 32, value size 8; .
+	pub const ProxyDepositBase: Balance = deposit(1, 8);
+	// Additional storage item size of 33 bytes.
+	pub const ProxyDepositFactor: Balance = deposit(0, 33);
+	pub const MaxProxies: u16 = 32;
+	pub const AnnouncementDepositBase: Balance = deposit(1, 8);
+	pub const AnnouncementDepositFactor: Balance = deposit(0, 66);
+	pub const MaxPending: u16 = 32;
 }
 
+/// The type used to represent the kinds of proxying allowed.
+#[derive(
+	Copy,
+	Clone,
+	Eq,
+	PartialEq,
+	Ord,
+	PartialOrd,
+	Encode,
+	Decode,
+	RuntimeDebug,
+	MaxEncodedLen,
+	scale_info::TypeInfo,
+)]
+pub enum ProxyType {
+	Any,
+	NonTransfer,
+	Governance,
+	Staking,
+}
+impl Default for ProxyType {
+	fn default() -> Self {
+		Self::Any
+	}
+}
+impl InstanceFilter<Call> for ProxyType {
+	fn filter(&self, c: &Call) -> bool {
+		match self {
+			ProxyType::Any => true,
+			ProxyType::NonTransfer => !matches!(
+				c,
+				Call::Balances(..) |
+					Call::Assets(..) | Call::Uniques(..) |
+					Call::Vesting(pallet_vesting::Call::vested_transfer { .. }) |
+					Call::Indices(pallet_indices::Call::transfer { .. })
+			),
+			ProxyType::Governance => matches!(
+				c,
+				Call::Democracy(..) |
+					Call::Council(..) | Call::Society(..) |
+					Call::TechnicalCommittee(..) |
+					Call::Elections(..) | Call::Treasury(..)
+			),
+			ProxyType::Staking => matches!(c, Call::Staking(..)),
+		}
+	}
+	fn is_superset(&self, o: &Self) -> bool {
+		match (self, o) {
+			(x, y) if x == y => true,
+			(ProxyType::Any, _) => true,
+			(_, ProxyType::Any) => false,
+			(ProxyType::NonTransfer, _) => true,
+			_ => false,
+		}
+	}
+}
 
+impl pallet_proxy::Config for Runtime {
+	type Event = Event;
+	type Call = Call;
+	type Currency = Balances;
+	type ProxyType = ProxyType;
+	type ProxyDepositBase = ProxyDepositBase;
+	type ProxyDepositFactor = ProxyDepositFactor;
+	type MaxProxies = MaxProxies;
+	type WeightInfo = pallet_proxy::weights::SubstrateWeight<Runtime>;
+	type MaxPending = MaxPending;
+	type CallHasher = BlakeTwo256;
+	type AnnouncementDepositBase = AnnouncementDepositBase;
+	type AnnouncementDepositFactor = AnnouncementDepositFactor;
+}
+// ====================> Pallet Scheduler <======================= //
+parameter_types! {
+	pub MaximumSchedulerWeight: Weight = Perbill::from_percent(80) *
+		RuntimeBlockWeights::get().max_block;
+	pub const MaxScheduledPerBlock: u32 = 50;
+}
+
+impl pallet_scheduler::Config for Runtime {
+	type Event = Event;
+	type Origin = Origin;
+	type PalletsOrigin = OriginCaller;
+	type Call = Call;
+	type MaximumWeight = MaximumSchedulerWeight;
+	type ScheduleOrigin = EnsureRoot<AccountId>;
+	type MaxScheduledPerBlock = MaxScheduledPerBlock;
+	type WeightInfo = pallet_scheduler::weights::SubstrateWeight<Runtime>;
+	type OriginPrivilegeCmp = EqualPrivilegeOnly;
+	
+//	========================> Pallet Collective <==================// 
+parameter_types! {
+	pub const CouncilMotionDuration: BlockNumber = 5 * DAYS;
+	pub const CouncilMaxProposals: u32 = 100;
+	pub const CouncilMaxMembers: u32 = 100;
+}
+
+type CouncilCollective = pallet_collective::Instance1;
+impl pallet_collective::Config<CouncilCollective> for Runtime {
+	type Origin = Origin;
+	type Proposal = Call;
+	type Event = Event;
+	type MotionDuration = CouncilMotionDuration;
+	type MaxProposals = CouncilMaxProposals;
+	type MaxMembers = CouncilMaxMembers;
+	type DefaultVote = pallet_collective::PrimeDefaultVote;
+	type WeightInfo = pallet_collective::weights::SubstrateWeight<Runtime>;
+}
+
+// =========================> END OF PALLET CONFIGURATION <==============================// 
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
@@ -339,6 +464,7 @@ construct_runtime!(
 		RecurringPaymentModule: pallet_recurringpayments,
 		Proxy: pallet_proxy,
 		Scheduler: pallet_scheduler,
+		Collective: pallet_collective,
 
 		
 	}
